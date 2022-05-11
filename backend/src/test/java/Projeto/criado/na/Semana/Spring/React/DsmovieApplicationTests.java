dcd33d1dddbff1090b807e1db7fc396500a5f8db
erik <erik.sgodoy@gmail.com>
2022-05-10 23:15:22 -0300

    Project Created

---
+++ backend/src/test/java/Projeto/criado/na/Semana/Spring/React/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.criado.na.Semana.Spring.React;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
